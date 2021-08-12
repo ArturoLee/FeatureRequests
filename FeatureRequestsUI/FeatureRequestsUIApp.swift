68771387c833c6e7c45894232ab7fa85f2c14ec1
ArturoLee <arturo9419@gmail.com>
2021-08-12 14:17:58 -0400

    Initial Commit

---
+++ FeatureRequestsUI/FeatureRequestsUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeatureRequestsUIApp.swift
+//  FeatureRequestsUI
+//
+//  Created by Arturo Lee on 8/12/21.
+//
+
+import SwiftUI
+
+@main
+struct FeatureRequestsUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
